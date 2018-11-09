441775dbc7caf2bec995e031dd797cb8021c2eab
Steven <propers@wwu.edu>
2018-11-08 19:49:05 -0800

    added fragments

---
+++ app/src/main/java/edu/wwu/csci412/a4/GameController.java
@@ -0,0 +1,20 @@
+package edu.wwu.csci412.a4;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class GameController extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_controller, container, false);
+    }
+
+}
---
+++ app/src/main/java/edu/wwu/csci412/a4/GameInterface.java
@@ -0,0 +1,19 @@
+package edu.wwu.csci412.a4;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class GameInterface extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_interface, container, false);
+    }
+}
